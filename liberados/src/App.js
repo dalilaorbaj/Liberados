63d651a85cec46a5f104228e878ff4de202ec59e
dalilaorbaj <dali.orbaj24@gmail.com>
2022-08-07 16:02:35 -0300

    Rutas definidas

--- liberados/src/App.js
+++ liberados/src/App.js
@@ -2,7 +2,10 @@ import Header from './Components/Header';
 import Navbar from './Components/Navbar';
 import ContentHome from './Components/ContentHome';
 import Footer from './Components/Footer';
+import Juego from '../Pages/Question'
+import Dashboard from '../Pages/Dashboard'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
@@ -12,6 +15,13 @@ function App() {
       <Header />
       <ContentHome />
       <Footer />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/jugar" component={Juego} />
+          <Route path="/resultados" component={Dashboard} />
+        </Switch>
+      </Router>
     </div>
   );
 }
