b2ab1cfe91e08a47ddcb15f7479f35e20fe8c115
dalilaorbaj <dali.orbaj24@gmail.com>
2022-08-07 17:43:10 -0300

    banner puesto

--- liberados/src/Components/Header.js
+++ liberados/src/Components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (    
             <div id="header" className="row justify-content-md-center">
                 <div className="mb-5 mt-5 col-6 justify-content-md-center">
-                    <noscript>ACA VA LA IMAGEN DEL BANNER</noscript>
+                    <img src={require('../Banner.png')} alt="Banner" style={{height: 300, flex: 1, width: null,}} />
                 </div>
             </div>        
     );
--- liberados/src/Pages/Home.js
+++ liberados/src/Pages/Home.js
@@ -2,6 +2,8 @@ import Header from '../Components/Header';
 import Navbar from '../Components/Navbar';
 import ContentHome from '../Components/ContentHome';
 import Footer from '../Components/Footer';
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 
