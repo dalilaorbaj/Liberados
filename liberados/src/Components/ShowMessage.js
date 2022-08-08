4145e9a088f2e3dfc98e6ca8da1d4aab6d202b80
Android Studio <devandroid@ort>
2022-08-08 10:20:13 -0300

    Integracion falta ruta jugar y seeResults

---
+++ liberados/src/Components/ShowMessage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ShowMessage = ({ avg }) => {
+   let message = {
+      text: '',
+      emoji: ''
+   }
+
+   if (avg >= 80) {
+      message.text = 'Excellent Work!';
+      message.emoji = '🎉';
+   }
+   else if (avg >= 70) {
+      message.text = 'Good Work!';
+      message.emoji = '🥂'
+   }
+   else {
+      message.text = 'Seguí practicando';
+      message.emoji = '💪'
+   }
+
+   return (
+      <>
+         <h1 className='emoji'>{message.emoji}</h1>
+         <h1>{message.text}</h1>
+      </>
+   )
+}
+
+export default ShowMessage
--- liberados/src/Pages/Question.js
+++ liberados/src/Pages/Question.js
@@ -1,18 +1,121 @@
-import Navbar from '../Components/Navbar';
+/*import Navbar from '../Components/Navbar';
 import Pregunta from '../Components/Pregunta';
-import Options from '../Components/Options';
+import Options from '../Components/Options';*/
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
-
-function Question({idQuestion}) {
-    return (
-      <div className="App">
-        <Navbar />
-        <Pregunta />
-        <noscript>HAY QUE MANDARLE EL CONTENT A LAS OPTIONS</noscript>
-        <Options />
-      </div>
-    );
+  import React, { useState } from 'react'
+  import styled from 'styled-components'
+  import { randQustions } from '../Components/Quiz'
+  import ShowMessage from '../Components/ShowMessage';
+  import { Box, Button } from '../Components/UIElements';
+  import ShowConfetti from '../Components/ShowConfetti';
+  const BasicGrid = styled.div`
+     display: grid;
+     gap: 1rem;
+     margin: 1rem 0;
+  `
+  const Question = () => {
+     const [currentIndex, setCurrentIndex] = useState(0)
+     const [endQuiz, setEndQuiz] = useState(false)
+     const [score, setScore] = useState(0);
+     const [avg, setAvg] = useState(0);
+     const [showAns, setShowAns] = useState(false);
+  
+     //function to calc score and show correct answer
+     const handleAnswerClick = (isCorrect, e) => {
+        setShowAns(true)
+        if (isCorrect) {
+           setScore((prev) => prev + 1);
+        }
+        /*setTimeout(() => {
+           nextQuestion()
+        }, 1800)*/
+     }
+  
+     //function to go to next question if within bounds of quiz length and hide correct answer 
+     // if ouside the bounds of the quiz array, the quiz will end
+     const nextQuestion = () => {
+        setShowAns(false)
+        if (currentIndex === randQustions.length - 1) {
+           endOfQuiz()
+        }
+        else {
+           setCurrentIndex(prev => prev + 1);
+        }
+     }
+  
+     //function to pre-emptively end quiz
+     const seeResults = () => {
+        setEndQuiz(true)
+        setAvg((score / currentIndex) * 100)
+     }
+  
+     const endOfQuiz = () => {
+        setEndQuiz(true)
+        setCurrentIndex(prev => prev + 1)
+        setAvg((score / currentIndex) * 100)
+     }
+  
+     //function to result all variables 
+     const reset = () => {
+        setEndQuiz(false);
+        setCurrentIndex(0);
+        setScore(0)
+        setShowAns(false)
+     }
+     if (endQuiz) {
+        return (
+           <>
+              {avg >= 80 &&
+                 <ShowConfetti whenToShow={endQuiz} />
+              }
+              <Box >
+                 <div className="spacer"></div>
+                 <div className="top" style={{ textAlign: 'center' }}>
+                    <ShowMessage avg={avg} />
+                    <p>Hiciste <strong>{score}</strong> bien de <strong>{currentIndex}</strong> = <strong>{avg.toFixed(1)}%</strong></p>
+                 </div>
+                 <Button onClick={reset} >Start over?</Button>
+              </Box>
+           </>
+        )
+     }
+  
+     return (
+        <>
+           <Box className='question-box m-full-y' >
+              <div className="top">
+                 <h6 className='top-text'>Pregunta {currentIndex + 1} de {randQustions.length}</h6>
+                 <div className="question">
+                    <h2 className='question-text'>{randQustions[currentIndex].question}</h2>
+                 </div>
+              </div>
+  
+              <BasicGrid className="answers-row middle">
+                 {randQustions[currentIndex].answers.map((answer, key) => (
+                    <Button
+                       className={showAns && answer.isCorrect ? 'ans' : ''}
+                       onClick={(e) => { handleAnswerClick(answer.isCorrect, e) }}
+                       key={key}
+                       disabled={showAns}
+                    >
+                       {answer.answerText}
+                    </Button>
+                 ))}
+              </BasicGrid>
+  
+              <div className="bottom">
+                 <Button
+                    className='secondary'
+                    onClick={nextQuestion()}
+                    disabled={showAns || currentIndex === 0}>
+                    Ver resultados
+                 </Button>
+              </div>
+           </Box>
+        </>
+     )
   }
+  
   
   export default Question;
