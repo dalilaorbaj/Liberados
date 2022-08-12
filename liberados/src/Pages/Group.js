dfbea47cf866ae694763d0170bdcaf12f3146556
dalilaorbaj <dalila.orbaj@gmail.com>
2022-08-12 11:53:53 -0300

    starting with group input

---
+++ liberados/src/Pages/Group.js
@@ -0,0 +1,18 @@
+import Navbar from '../Components/Navbar';
+import ContentHome from '../Components/ContentHome';
+import Footer from '../Components/Footer';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+const Group = () => {
+    return (  
+    <>
+        <Navbar />
+        <Question />
+        <Footer />
+    </>  
+    )
+}
+
+export default Group;
