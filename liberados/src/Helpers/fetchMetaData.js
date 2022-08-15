38c502002c0cf73a61c6691763ea2eb1fec03682
Valentin Jacofsky <valentinjacofskyort@gmail.com>
2022-08-15 20:36:03 -0300

    fix

--- liberados/src/Helpers/fetchMetaData.js
+++ liberados/src/Helpers/fetchMetaData.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 const getData = async (grupo) =>{
-    const res = await axios.get(`https://liberados-api.herokuapp.com/preguntas/${grupo}`);
+    let res
+    if (!grupo) {
+        res = await axios.get(`https://liberados-api.herokuapp.com/preguntas`);
+    } else {
+        res = await axios.get(`https://liberados-api.herokuapp.com/preguntas/${grupo}`);
+
+    }
     const randQuestions = res.data.sort(() => Math.random() - 0.5)
     return randQuestions;
 }
