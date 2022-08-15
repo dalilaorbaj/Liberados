39b6c7a6e755cd958bcf120f0c40fc057138bbcd
dalilaorbaj <dali.orbaj24@gmail.com>
2022-08-15 17:19:03 -0300

    porfas

--- liberados/src/Components/QuestionGroup.js
+++ liberados/src/Components/QuestionGroup.js
@@ -15,19 +15,27 @@ const onChangeInput = (e) =>{
     setGrupo({...grupo, [e.target.value]:e.target.value})
 }
 */
+
+
+/*
 const handleSubmit = (e) =>{
-    e.preventDefault()
+    e.preventDefault();
     setGrupo(e);
   }
+  */
+
+const handleMensaje = (grupo) => {
+    setGrupo(grupo)
+}
 
         return (
           <>        
           <div className="centrado">  
           <h2>¿Cómo se llama tu grupo?</h2>
-          <form >
-              <input id="nombre" type="text" name="grupo" className="u-full-width mb-3" placeholder="Nombre del grupo" value={grupo}/> <br/>
+          <form>
+              <input id="nombre" type="text" name="grupo" className="u-full-width mb-3" placeholder="Nombre del grupo"  onChangeText={(grupo) => handleMensaje(grupo)}/> <br/>
               <Link to="/jugar">
-                <button className="buttonForm" id="btn-sbmt" onClick={handleSubmit}>Enviar</button>
+                <button className="buttonForm" id="btn-sbmt">Enviar</button>
               </Link>  
           </form>
           </div>
