e3757c0291e31add66ff6f6a1042f9ea2bd349a2
Valentin Jacofsky <valentinjacofskyort@gmail.com>
2022-08-15 17:28:59 -0300

    fix

--- liberados/src/Components/QuestionGroup.js
+++ liberados/src/Components/QuestionGroup.js
@@ -4,43 +4,25 @@ import '../global.css';
 const QuestionGroup = () => {
 
   const [grupo, setGrupo] = useState('');
-/*
-  const guardarGrupo = (value) =>{
-    const x = value;
-    setGrupo(x);
-  }
-*/
-/*
-const onChangeInput = (e) =>{
-    setGrupo({...grupo, [e.target.value]:e.target.value})
-}
-*/
 
-
-/*
-const handleSubmit = (e) =>{
-    e.preventDefault();
-    setGrupo(e);
-  }
-  */
-
-const handleMensaje = (grupo) => {
+  const handleMensaje = (grupo) => {
     setGrupo(grupo)
-}
+  }
 
-        return (
-          <>        
-          <div className="centrado">  
-          <h2>¿Cómo se llama tu grupo?</h2>
-          <form>
-              <input id="nombre" type="text" name="grupo" className="u-full-width mb-3" placeholder="Nombre del grupo"  onChangeText={(grupo) => handleMensaje(grupo)}/> <br/>
-              <Link to="/jugar">
-                <button className="buttonForm" id="btn-sbmt">Enviar</button>
-              </Link>  
-          </form>
-          </div>
-      </>
-        );
-      }
+  return (
+    <>        
+      <div className="centrado">  
+        <h2>¿Cómo se llama tu grupo?</h2>
+        <form>
+
+          <input id="nombre" type="text" name="grupo" className="u-full-width mb-3" placeholder="Nombre del grupo" value={grupo} onChange={() => console.log()}/> <br/>
+          
+          <button className="buttonForm" id="btn-sbmt">Enviar</button>
+
+        </form>
+      </div>
+    </>
+  );
+}
 
 export default QuestionGroup;
