23af109290d088f6f359f89062782290f27bc080
dalilaorbaj <dalila.orbaj@gmail.com>
2022-08-16 09:01:40 -0300

    All done

--- liberados/src/Components/ContentHome.js
+++ liberados/src/Components/ContentHome.js
@@ -6,7 +6,8 @@ const ContentHome = () => {
     return (    
             <div className="justify-content-md-center">
                 <br/>
-                <h5 className="mb-4">Aca va un textito sobre el juegoooooo</h5>
+                <h5 className="mb-4">La revolución libertadora tuvo particularidades muy interesantes.</h5> 
+                <h5 style={{marginBottom:"50px"}}>Animate a responder las siguientes preguntas generadas por tus compañeros y compañeras y ¡liberá América!</h5>
                 <Link to="/grupo">
                     <button className="niceButton" style={{width: 200, height: 60}}>
                         <p style={{fontSize: 40, marginTop: "5%"}}> Jugar </p>
